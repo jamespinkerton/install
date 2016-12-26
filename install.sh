66a21ebfb28318b9d5bb8304ee0a06e909165213
EC2 Default User <ec2-user@ip-172-31-62-108.ec2.internal>
2016-12-26 16:58:31 -0500

    patches up here and there

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+set -o errexit
 # sudo yum groupinstall 'Development Tools' && sudo yum install curl git irb python-setuptools ruby
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" || echo "Brew already installed."
 # This is equivalent to the above:
 # git clone https://github.com/Linuxbrew/linuxbrew.git ~/.linuxbrew
 export PATH=$HOME/.linuxbrew/bin:$PATH
@@ -13,3 +14,4 @@ git config --global credential.helper 'cache --timeout=3600'
 GITDIR=$(dirname $(realpath $0))
 /bin/bash $GITDIR/link.sh
 source ~/.bashrc
+vi +Pluginstall +qa
