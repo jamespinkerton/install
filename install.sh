688fbf073ef389b7d7ff0f177e88c836c75a3728
EC2 Default User <ec2-user@ip-172-31-62-108.ec2.internal>
2016-12-28 01:14:08 -0500

    Get python3 to work

--- install.sh
+++ install.sh
@@ -4,16 +4,19 @@ set -o errexit -o nounset
 GITDIR=$(cd $(dirname $0) && pwd)
 $GITDIR/link.sh
 
-PACKAGES="git hg tmux wget curl htop python python35 vim emacs"
+PACKAGES="git hg tmux wget curl htop python vim emacs"
 if [[ -n "$(command -v apt-get)" ]]; then
+    sudo apt-get install python3
     sudo apt-get install $PACKAGES mailutils
 elif [[ -n "$(command -v yum)" ]]; then
+    sudo yum -y update
     sudo yum -y install epel-release
+    sudo yum -y install python35 python35-setuptools
+    sudo easy_install-3.5 pip
     sudo yum -y install $PACKAGES mailx
 fi
 
-pip3 install --upgrade pip
-pip3 install scipy numpy pandas scikit-learn ipython seaborn matplotlib mypy-lang powerline-status
+pip3 install --user scipy numpy pandas scikit-learn ipython seaborn matplotlib mypy-lang powerline-status
 
 source ~/.bashrc
 
