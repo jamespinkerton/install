edca99b278df102503fe426a8015921a1c05b537
James Pinkerton <jamescpinkerton@gmail.com>
2017-07-07 00:34:51 +0000

    Used ssh

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,4 +1,4 @@
-git clone --bare https://github.com/jamespinkerton/dotfiles.git $HOME/.cfg
+git clone --bare git@github.com:jamespinkerton/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
