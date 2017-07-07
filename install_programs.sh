da3ad7b52f48d56fe48c2107c0cd95dcbf022ff0
James Pinkerton <jamescpinkerton@gmail.com>
2017-07-07 00:51:46 +0000

    Move some stuff around in install

--- install_programs.sh
+++ install_programs.sh
@@ -10,23 +10,26 @@ elif [[ -n "$(command -v yum)" ]]; then
 fi
 
 [[ ! -d /home/linuxbrew/.linuxbrew ]] && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-brew install gcc git hg tmux wget htop emacs curl vim || echo "Stuff was already installed?"
-
-# Not working righ now
-# brew install neovim # Won't work in redhat for some reason???
-# brew install mailutils
-# brew install gawk glibc # gawk is a prereq but seems to have problems in debian
-# brew install texlive && tlmgr update --self && tlmgr install texliveonfly
 
 if [[ ! -d ~/miniconda3 ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -P ~ -O miniconda.sh
     /bin/bash ~/miniconda.sh -b
     rm -f ~/miniconda.sh
 fi
+# Not working:
+# bash "$(curl -fsSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh)" -b
+
+
+export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+brew install gcc git hg tmux wget htop emacs curl vim || echo "Stuff was already installed?"
+# Not working righ now
+# brew install neovim # Won't work in redhat for some reason???
+# brew install mailutils
+# brew install gawk glibc # gawk is a prereq but seems to have problems in debian
+# brew install texlive && tlmgr update --self && tlmgr install texliveonfly
+
 export PATH=$HOME/miniconda3/bin:$PATH
 conda update --all python=3 --yes
 conda install anaconda --yes #Will anaconda be updated?
 
 vi +PlugInstall +qa # Won't work in redhat for some reason???
-source ~/.bash_profile
