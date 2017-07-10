313ebdc1bfe0bd9c6ccc82320b8249195d8fdfbb
James Pinkerton <jamespinkerton@users.noreply.github.com>
2017-07-10 14:05:37 -0700

    Update install_programs.sh

--- install_programs.sh
+++ install_programs.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 set -o errexit -o nounset -o xtrace
 
+# ****** Install Linuxbrew ******
 if [[ -n "$(command -v apt-get)" ]]; then
     sudo apt-get -y update
-    sudo apt-get -y install build-essential curl git python-setuptools ruby
+    sudo apt-get -y install build-essential curl file git python-setuptools ruby
 elif [[ -n "$(command -v yum)" ]]; then
     sudo yum -y update
     sudo yum groupinstall 'Development Tools' && sudo yum install curl git irb python-setuptools ruby
 fi
 
 [[ ! -d /home/linuxbrew/.linuxbrew ]] && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-
-if [[ ! -d ~/miniconda3 ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-    /bin/bash ~/miniconda.sh -b
-    rm -f ~/miniconda.sh
-fi
-# Not working:
-# bash "$(curl -fsSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh)" -b
-
 
 export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
 brew install gcc git hg tmux wget htop emacs curl vim || echo "Stuff was already installed?"
@@ -28,8 +20,19 @@ brew install gcc git hg tmux wget htop emacs curl vim || echo "Stuff was already
 # brew install gawk glibc # gawk is a prereq but seems to have problems in debian
 # brew install texlive && tlmgr update --self && tlmgr install texliveonfly
 
+# ****** Install MiniConda ******
+if [[ ! -d ~/miniconda3 ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+    /bin/bash ~/miniconda.sh -b
+    rm -f ~/miniconda.sh
+fi
+# Not working:
+# bash "$(curl -fsSL https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh)" -b
+
 # export PATH=$HOME/miniconda3/bin:$PATH
 # conda update --all python=3 --yes
 # conda install anaconda --yes #Will anaconda be updated?
 
+# ********** Misc. **********
+source ~/.bash_profile
 vi +PlugInstall +qa # Won't work in redhat for some reason???
