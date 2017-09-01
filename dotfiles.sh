6022af56d18915b3372463aadb6c471fbeb3b512
James Pinkerton <jamescpinkerton@gmail.com>
2017-09-01 11:26:28 -0700

    Make dotfiles repo a variable

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,7 +5,8 @@
 
 set -o errexit -o nounset -o xtrace
 
-git clone --bare git@github.com:jamespinkerton/dotfiles.git $HOME/.cfg
+DOTFILES_REPO=git@github.com:jamespinkerton/dotfiles.git
+git clone --bare $DOTFILES_REPO $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME config status.showUntrackedFiles no
 
