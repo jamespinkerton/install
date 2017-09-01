3cd1923c42534ef65812546eb5762469f974d65e
James Pinkerton <jamescpinkerton@gmail.com>
2017-09-01 13:13:20 -0700

    remove work tree from config setting

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,7 +7,7 @@ set -o errexit -o nounset -o xtrace
 
 DOTFILES_REPO=git@github.com:jamespinkerton/dotfiles.git
 git clone --bare $DOTFILES_REPO $HOME/.cfg
+git --git-dir=$HOME/.cfg/ config status.showUntrackedFiles no
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME config status.showUntrackedFiles no
 
 source ~/.bash_profile
