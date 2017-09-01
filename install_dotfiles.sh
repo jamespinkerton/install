d086d4224b91a9b68844a81aba46a07906ef61a0
James Pinkerton <jamespinkerton@fb.com>
2017-09-01 11:05:36 -0700

    remove unecessary backup code (trust revision control both for recovering this code and for the idea this code is unecessary)

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,16 +1,6 @@
+# Inspiration is from here:
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 
 git clone --bare git@github.com:jamespinkerton/dotfiles.git $HOME/.cfg
-function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-mkdir -p $HOME/.config-backup
-echo "Backing up pre-existing dot files."
-FILES="$(config checkout 2>&1 | egrep "\s+\." | awk {'print $1'})"
-for element in $FILES; do
-    dest=$HOME/.config-backup/$element
-    mkdir --parents "$(dirname $dest)" 
-    mv $HOME/$element $dest
-done
-config checkout
-config config status.showUntrackedFiles no
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME config status.showUntrackedFiles no
