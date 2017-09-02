255bca176a043d91425823b1010ac3b042d11e01
James Pinkerton <jamescpinkerton@gmail.com>
2017-09-02 05:51:58 +0000

    Force checkout and add comment about alias in dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,11 +3,14 @@
 # Inspiration is from here:
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 
+# Put this in your bashrc. Use config instead of git to refer to your dotfiles repo.
+# alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
 set -o errexit -o nounset -o xtrace
 
 DOTFILES_REPO=$1
 git clone --bare $DOTFILES_REPO $HOME/.cfg
 git --git-dir=$HOME/.cfg/ config status.showUntrackedFiles no
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -f
 
 source ~/.bash_profile
