44a963c95b5428caa84a8ba5aadfc072349c58b0
James Pinkerton <jamescpinkerton@gmail.com>
2017-09-15 16:45:11 -0700

    CMAKE_BUILD_TYPE in neovim_source.sh

--- neovim_source.sh
+++ neovim_source.sh
@@ -4,5 +4,5 @@ set -o errexit -o nounset -o xtrace
 
 git clone https://github.com/neovim/neovim ~/packages/neovim
 cd ~/packages/neovim
-make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.neovim"
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.neovim" CMAKE_BUILD_TYPE=Release
 make install
